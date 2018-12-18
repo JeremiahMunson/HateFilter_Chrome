d2ccd07012acd3f735dc8a312553ddab55ce0bd8
Jeremiah Munson <j.munson225@gmail.com>
2018-12-18 13:46:35 -0600

    Did a lot. Got it to work on every website, not just developer.chrome.com

--- popup.js
+++ popup.js
@@ -6,13 +6,13 @@ activate.onclick = function(){
     if(!activated){
         activated = true;
         chrome.tabs.executeScript({
-            code: "pars = document.getElementsByTagName('p'); badPars = []; badDisp = []; for(var i = 0; i < pars.length; i++){pars[i].innerHTML = pars[i].innerHTML.toLowerCase(); if(pars[i].innerHTML.indexOf('api') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
+            code: "pars = document.getElementsByTagName('p'); badPars = []; badDisp = []; for(var i = 0; i < pars.length; i++){pars[i].innerHTML = pars[i].innerHTML.toLowerCase(); if(pars[i].innerHTML.indexOf('it') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
         });
         this.innerHTML = "Deactivate";
-    }else{
+    }
+    else{
         activated = false;
         chrome.tabs.executeScript({
-            //code: "for(var j = 0; j < badPars.length; j++){badPars[j,0].style.display = badPars[j,1];}"
             code: "for(var j = 0; j < badPars.length; j++){badPars[j].style.display = badDisp[j];}"
         });
         this.innerHTML = "Activate";
