597e9819cb190507f053584dc2892f78c91f93ff
Jeremiah Munson <j.munson225@gmail.com>
2018-12-18 12:40:23 -0600

    Made so when activated the popup shows Deactivate button and when clicked it shows the paragraphs that were hidden

--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 let activate = document.getElementById('activate');
 
-activate.onclick = function() {
-    chrome.tabs.executeScript({
-        /*code: 'paragraphs = document.getElementsByTagName("p"); badParagraphs = [];',
-        code: 'for (var i = 0; i < paragraphs.length; i++){paragraphs[i].innerHTML.toLowerCase(); if (paragraphs[i].innerHTML.indexOf("api") >= 0) {badParagraphs.push(paragraphs[i]);}}',
-        code: 'for(var j = 0; j < badParagraphs.length; j++){badParagraphs[j].style.display = "none";}',*/
-        code: "pars = document.getElementsByTagName('p'); for(var i = 0; i < pars.length; i++){pars[i].innerHTML = pars[i].innerHTML.toLowerCase(); if(pars[i].innerHTML.indexOf('api') >= 0){pars[i].style.display = 'none';}}"
-    });
+activated = false;
+
+activate.onclick = function(){
+    if(!activated){
+        activated = true;
+        chrome.tabs.executeScript({
+            code: "pars = document.getElementsByTagName('p'); badPars = []; badDisp = []; for(var i = 0; i < pars.length; i++){pars[i].innerHTML = pars[i].innerHTML.toLowerCase(); if(pars[i].innerHTML.indexOf('api') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
+        });
+        this.innerHTML = "Deactivate";
+    }else{
+        activated = false;
+        chrome.tabs.executeScript({
+            //code: "for(var j = 0; j < badPars.length; j++){badPars[j,0].style.display = badPars[j,1];}"
+            code: "for(var j = 0; j < badPars.length; j++){badPars[j].style.display = badDisp[j];}"
+        });
+        this.innerHTML = "Activate";
+    };
 };
