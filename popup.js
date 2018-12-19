417021d29eb478409edea07c2bb60ffdc7e0571b
Jeremiah Munson <j.munson225@gmail.com>
2018-12-18 18:43:58 -0600

    tying to add icons to extension

--- popup.js
+++ popup.js
@@ -10,18 +10,25 @@ activate.onclick = function(){
     chrome.storage.sync.get('activated', function(data){activated = data.activated;
         if(!activated){
             chrome.storage.sync.set({'activated': true});
-            chrome.tabs.executeScript({
-                code: "pars = document.getElementsByTagName('p'); lowerPars = []; badPars = []; badDisp = []; for(var i = 0; i < pars.length; i++){lowerPars.push(pars[i].innerHTML.toLowerCase()); if(lowerPars[i].indexOf('it') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
-            });
+            hide();
             activate.innerHTML = "Deactivate";
         }
         else{
             chrome.storage.sync.set({'activated': false});
-            chrome.tabs.executeScript({
-                code: "for(var j = 0; j < badPars.length; j++){badPars[j].style.display = badDisp[j];}"
-            });
+            show();
             activate.innerHTML = "Activate";
         };
     });
-    
+};
+
+function hide(){
+    chrome.tabs.executeScript({
+        code: "pars = document.getElementsByTagName('p'); lowerPars = []; badPars = []; badDisp = []; for(var i = 0; i < pars.length; i++){lowerPars.push(pars[i].innerHTML.toLowerCase()); if(lowerPars[i].indexOf('it') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
+    });
+};
+
+function show(){
+    chrome.tabs.executeScript({
+        code: "for(var j = 0; j < badPars.length; j++){badPars[j].style.display = badDisp[j];}"
+    });
 };
