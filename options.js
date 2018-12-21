7fd7f722a3ed57913f83cade0470fb7e46c4ef40
Jeremiah Munson <j.munson225@gmail.com>
2018-12-20 20:49:50 -0600

    Completely redid the adding of the words/phrases to block to the page and the printing of the error message for duplicate words. I also added pressing enter in the text boxes as adding the word/phrase (pressing enter clicks the add button)

--- options.js
+++ options.js
@@ -1,8 +1,14 @@
 var saveButton = document.getElementById('saveItems');
+
 var addHomophobic = document.getElementById('addHomophobic');
 var addRacist = document.getElementById('addRacist');
 var addSexist = document.getElementById('addSexist');
 var addOther2 = document.getElementById('addOther2');
+
+var homophobicTxt = document.getElementById('homophobicWord');
+var racistTxt = document.getElementById('racistWord');
+var sexistTxt = document.getElementById('sexistWord');
+var other2Txt = document.getElementById('other2Word');
 
 var homophobic = [];
 var homophobia = document.getElementById('homophobia');
@@ -13,13 +19,10 @@ var sexism = document.getElementById('sexism');
 var other2 = [];
 var other1 = document.getElementById('other1');
 
-var repeatHomophobic = false;
-var repeatRacist = false;
-var repeatSexist = false;
-var repeatOther2 = false;
-
+var begin = true;
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab){
-    if(changeInfo.status == 'complete' && tab.active){
+    if (changeInfo.status == 'complete' && tab.active && begin) {
+        begin = false;
         chrome.storage.sync.get('homophobic', function(storedHomophobic){
             homophobicWords = storedHomophobic.homophobic;
             for(var i = 0; i < homophobicWords.length; i++){
@@ -60,7 +63,7 @@ chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab){
                 var lab = document.createElement("label");
                 var input = document.createElement("input");
                 input.type = "checkbox";
-                input.class = "sexist";
+                input.class = "sexist"; 
                 input.value = sexistWords[i];
                 var node = document.createTextNode(sexistWords[i]);
                 sexist.push(input);
@@ -112,36 +115,50 @@ chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab){
                 other2[i].checked = false;
                 for(var j = 0; j < blockedWords.length; j++){
                     if(other2[i].value == blockedWords[j]){other2[i].checked = true;};
-                };
+                 };
             };
         });
-
-        chrome.storage.sync.get('repeatHomophobic', function(repeat){
-            if(!repeat.repeatHomophobic){document.getElementById('HomophobicRepeat').style.display = "none";}
-            else{document.getElementById('HomophobicRepeat').style.display = "block";};
-        });
-        chrome.storage.sync.get('repeatRacist', function(repeat){
-            if(!repeat.repeatRacist){document.getElementById('RacistRepeat').style.display = "none";}
-            else{document.getElementById('RacistRepeat').style.display = "block";};
-        });
-        chrome.storage.sync.get('repeatSexist', function(repeat){
-            if(!repeat.repeatSexist){document.getElementById('SexistRepeat').style.display = 'none';}
-            else{document.getElementById('SexistRepeat').style.display = 'block';};
-        });
-        chrome.storage.sync.get('repeatOther2', function(repeat){
-            if(!repeat.repeatOther2){document.getElementById('Other2Repeat').style.display = 'none';}
-            else{document.getElementById('Other2Repeat').style.display = 'block';};
-        })
+        document.getElementById('HomophobicRepeat').style.display = "none";
+        document.getElementById('RacistRepeat').style.display = "none";
+        document.getElementById('SexistRepeat').style.display = 'none';
+        document.getElementById('Other2Repeat').style.display = 'none';
     };
 });
 
+homophobicTxt.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        addHomophobic.click();
+    }
+});
+
+racistTxt.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if(event.keyCode === 13){
+        addRacist.click();
+    }
+});
+
+sexistTxt.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if(event.keyCode === 13){
+        addSexist.click();
+    }
+});
+
+other2Txt.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if(event.keyCode === 13){
+        addOther2.click();
+    }
+});
 
 saveButton.onclick = function(){
     var wordsToBlock = [];
-    chrome.storage.sync.set({'repeatHomophobic':false});
-    chrome.storage.sync.set({'repeatRacist':false});
-    chrome.storage.sync.set({'repeatSexist':false});
-    chrome.storage.sync.set({'repeatOther2':false});
+    document.getElementById('HomophobicRepeat').style.display = "none";
+    document.getElementById('RacistRepeat').style.display = "none";
+    document.getElementById('SexistRepeat').style.display = 'none';
+    document.getElementById('Other2Repeat').style.display = 'none';
 
     for(var i = 0; i < homophobic.length; i++){
         if(homophobic[i].checked){
@@ -169,43 +186,61 @@ saveButton.onclick = function(){
 addHomophobic.onclick = function(){
     var homophobicToAdd = document.getElementById('homophobicWord').value.toLowerCase();
     var wordRepeat = false;
-    chrome.storage.sync.set({'repeatRacist':false});
-    chrome.storage.sync.set({'repeatSexist':false});
-    chrome.storage.sync.set({'repeatOther2':false});
+    document.getElementById('RacistRepeat').style.display = "none";
+    document.getElementById('SexistRepeat').style.display = 'none';
+    document.getElementById('Other2Repeat').style.display = 'none';
 
     chrome.storage.sync.get('homophobic', function(homophobicWords){
         var words = homophobicWords.homophobic;
         for(var i = 0; i < words.length; i++){
             if(homophobicToAdd == words[i]){
                 wordRepeat = true;
+                document.getElementById('HomophobicRepeat').style.display = "block";
                 break;
             };
         };
         if(!wordRepeat){
             words.push(homophobicToAdd);
             chrome.storage.sync.set({'homophobic': words});
+            document.getElementById('HomophobicRepeat').style.display = "none";
+        
+            var lab = document.createElement("label");
+            var input = document.createElement("input");
+            input.type = "checkbox";
+            input.class = "homophobic";
+            input.value = homophobicToAdd;
+            input.checked = true;
+            var node = document.createTextNode(homophobicToAdd);
+            homophobic.push(input);
+            lab.appendChild(input);
+            lab.appendChild(node);
+            homophobia.appendChild(lab);
+            homophobia.appendChild(document.createElement("br"));
+
+            labels = document.getElementsByTagName("label");
+
+            var homophobicLabels = document.getElementsByClassName('homophobic');
         };
-        chrome.storage.sync.set({'repeatHomophobic': wordRepeat});
     });
     chrome.storage.sync.get('block', function(blocking){
         var block = blocking.block;
         block.push(homophobicToAdd);
         chrome.storage.sync.set({'block': block});
     });
-    chrome.tabs.reload();
 };
 
 addRacist.onclick = function(){
     var racistToAdd = document.getElementById('racistWord').value.toLowerCase();
     var wordRepeat = false;
-    chrome.storage.sync.set({'repeatHomophobic':false});
-    chrome.storage.sync.set({'repeatSexist':false});
-    chrome.storage.sync.set({'repeatOther2':false});
+    document.getElementById('HomophobicRepeat').style.display = "none";
+    document.getElementById('SexistRepeat').style.display = 'none';
+    document.getElementById('Other2Repeat').style.display = 'none';
 
     chrome.storage.sync.get('racist', function(racistWords){
         var words = racistWords.racist;
         for(var i = 0; i < words.length; i++){
             if(racistToAdd == words[i]){
+                document.getElementById('RacistRepeat').style.display = "block";
                 wordRepeat = true;
                 break;
             };
@@ -213,71 +248,121 @@ addRacist.onclick = function(){
         if(!wordRepeat){
             words.push(racistToAdd);
             chrome.storage.sync.set({'racist':words});
+            document.getElementById('RacistRepeat').style.display = "none";
+        
+            var lab = document.createElement("label");
+            var input = document.createElement("input");
+            input.type = "checkbox";
+            input.class = "racist";
+            input.value = racistToAdd;
+            input.checked = true;
+            var node = document.createTextNode(racistToAdd);
+            racist.push(input);
+            lab.appendChild(input);
+            lab.appendChild(node);
+            racism.appendChild(lab);
+            racism.appendChild(document.createElement("br"));
+
+            labels = document.getElementsByTagName("label");
+
+            var racistLabels = document.getElementsByClassName('racist');
         };
-        chrome.storage.sync.set({'repeatRacist': wordRepeat});
     });
     chrome.storage.sync.get('block', function(blocking){
         var block = blocking.block;
         block.push(racistToAdd);
         chrome.storage.sync.set({'block': block});
     });
-    chrome.tabs.reload();
 };
 
 addSexist.onclick = function(){
     var sexistToAdd = document.getElementById('sexistWord').value.toLowerCase();
     var wordRepeat = false;
-    chrome.storage.sync.set({'repeatHomophobic':false});
-    chrome.storage.sync.set({'repeatRacist':false});
-    chrome.storage.sync.set({'repeatOther2':false});
+    document.getElementById('HomophobicRepeat').style.display = "none";
+    document.getElementById('RacistRepeat').style.display = "none";
+    document.getElementById('Other2Repeat').style.display = 'none';
 
     chrome.storage.sync.get('sexist', function(sexistWords){
         var words = sexistWords.sexist;
         for(var i = 0; i < words.length; i++){
             if(sexistToAdd == words[i]){
                 wordRepeat = true;
+                document.getElementById('SexistRepeat').style.display = 'block';
                 break;
             };
         };
         if(!wordRepeat){
             words.push(sexistToAdd);
             chrome.storage.sync.set({'sexist':words});
+            document.getElementById('SexistRepeat').style.display = 'none';
+        
+            var lab = document.createElement("label");
+            var input = document.createElement("input");
+            input.type = "checkbox";
+            input.class = "sexist";
+            input.value = sexistToAdd;
+            input.checked = true;
+            var node = document.createTextNode(sexistToAdd);
+            sexist.push(input);
+            lab.appendChild(input);
+            lab.appendChild(node);
+            sexism.appendChild(lab);
+            sexism.appendChild(document.createElement("br"));
+
+            labels = document.getElementsByTagName("label");
+
+            var sexistLabels = document.getElementsByClassName('sexist');
         };
-        chrome.storage.sync.set({"repeatSexist": wordRepeat});
     });
     chrome.storage.sync.get('block', function(blocking){
         var block = blocking.block;
         block.push(sexistToAdd);
         chrome.storage.sync.set({'block': block});
     });
-    chrome.tabs.reload();
 };
 
 addOther2.onclick = function(){
     var other2ToAdd = document.getElementById('other2Word').value.toLowerCase();
     var wordRepeat = false;
-    chrome.storage.sync.set({'repeatHomophobic':false});
-    chrome.storage.sync.set({'repeatRacist':false});
-    chrome.storage.sync.set({'repeatSexist':false});
+    document.getElementById('HomophobicRepeat').style.display = "none";
+    document.getElementById('RacistRepeat').style.display = "none";
+    document.getElementById('SexistRepeat').style.display = 'none';
 
     chrome.storage.sync.get('other2', function(other2Words){
         var words = other2Words.other2;
         for(var i = 0; i < words.length; i++){
             if(other2ToAdd == words[i]){
                 wordRepeat = true;
+                document.getElementById('Other2Repeat').style.display = 'block';
                 break;
             };
         };
         if(!wordRepeat){
             words.push(other2ToAdd);
             chrome.storage.sync.set({'other2':words});
+            document.getElementById('Other2Repeat').style.display = 'none';
+        
+            var lab = document.createElement("label");
+            var input = document.createElement("input");
+            input.type = "checkbox";
+            input.class = "other2";
+            input.value = other2ToAdd;
+            input.checked = true;
+            var node = document.createTextNode(other2ToAdd);
+            other2.push(input);
+            lab.appendChild(input);
+            lab.appendChild(node);
+            other1.appendChild(lab);
+            other1.appendChild(document.createElement("br"));
+            
+            labels = document.getElementsByTagName("label");
+
+            var other2Labels = document.getElementsByClassName('other2');
         };
-        chrome.storage.sync.set({"repeatOther2": wordRepeat});
     });
     chrome.storage.sync.get('block', function(blocking){
         var block = blocking.block;
         block.push(other2ToAdd);
         chrome.storage.sync.set({'block':block});
     });
-    chrome.tabs.reload();
 };
