743b08ebfbfcd07429c3a9101fbd7b9bdd64dddb
Jeremiah Munson <j.munson225@gmail.com>
2018-12-27 09:39:00 -0600

    chaged format of the code in executeScript for hiding content. This is easier to read, not all on one line.

--- background.js
+++ background.js
@@ -32,55 +32,158 @@ chrome.webNavigation.onCompleted.addListener(function(){
 function hide(block){
     // Setting the arrays to be empty. Have to do it before the loop otherwise the arrays before the last loop will be lost because they'll be reset to empty each loop.
     chrome.tabs.executeScript({
-        code: "lowerPars = []; badPars = []; badDisp = []; lowerComs = []; badComs = []; badComD = []; lowerSubComs = []; badSubComs = []; badSubComD = []; lowerDD = []; badDD = []; badDDisp = []; lowerDT = []; badDT = []; badTDisp = []; lowerQT = []; badQT = []; badQTDisp = []; lowerH1 = []; badH1 = []; badH1Disp = []; lowerYTD = []; badYTD = []; badYTDDisp = []; lowerYTC = []; badYTC = []; badYTCDisp = []; lowerTable = []; badTable = []; badTableDisp = [];"
+        code:   "lowerPars = []; badPars = []; badDisp = [];" +
+                "lowerComs = []; badComs = []; badComD = [];" +
+                "lowerSubComs = []; badSubComs = []; badSubComD = [];" +
+                "lowerDD = []; badDD = []; badDDisp = [];" +
+                "lowerDT = []; badDT = []; badTDisp = [];" +
+                "lowerQT = []; badQT = []; badQTDisp = [];" +
+                "lowerH1 = []; badH1 = []; badH1Disp = [];" +
+                "lowerYTD = []; badYTD = []; badYTDDisp = [];" +
+                "lowerYTC = []; badYTC = []; badYTCDisp = [];" +
+                "lowerTable = []; badTable = []; badTableDisp = [];" +
+                "lowerTableHead = []; badTableHead = []; badTableHeadDisp = [];"
     });
     // Looping through all words/phrases to block
     for(var i = 0; i < block.length; i++){
         // Checking paragraphs
         chrome.tabs.executeScript({
-            code: "pars = document.getElementsByTagName('p');  for(var i = 0; i < pars.length; i++){lowerPars.push(pars[i].innerHTML.toLowerCase()); if(lowerPars[i].indexOf('" + block[i] + "') >= 0){ badPars.push(pars[i]); badDisp.push(pars[i].style.display); pars[i].style.display = 'none';}}"
+            code:   "pars = document.getElementsByTagName('p');" +
+                    "for(var i = 0; i < pars.length; i++){" +
+                    "   lowerPars.push(pars[i].innerHTML.toLowerCase());" +
+                    "   if(lowerPars[i].indexOf('" + block[i] + "') >= 0){" +
+                    "       badPars.push(pars[i]);" +
+                    "       badDisp.push(pars[i].style.display);" +
+                    "       pars[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Checking Facebook comments
         chrome.tabs.executeScript({
-            code: "fbComments = document.querySelectorAll('[aria-label = \"Comment\"]'); for (var i = 0; i < fbComments.length; i++){lowerComs.push(fbComments[i].innerHTML.toLowerCase()); if(lowerComs[i].indexOf('" + block[i] + "') >= 0){badComs.push(fbComments[i]); badComD.push(fbComments[i].style.display); fbComments[i].style.display = 'none';}}"
+            code:   "fbComments = document.querySelectorAll('[aria-label = \"Comment\"]');" +
+                    "for (var i = 0; i < fbComments.length; i++){" +
+                    "   lowerComs.push(fbComments[i].innerHTML.toLowerCase());" +
+                    "   if(lowerComs[i].indexOf('" + block[i] + "') >= 0){" +
+                    "       badComs.push(fbComments[i]);" +
+                    "       badComD.push(fbComments[i].style.display);" +
+                    "       fbComments[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Checking Facebook comment replies
         chrome.tabs.executeScript({
-            code: "fbSubComments = document.querySelectorAll('[aria-label = \"Comment reply\"]'); for (var i = 0; i < fbSubComments.length; i++){lowerSubComs.push(fbSubComments[i].innerHTML.toLowerCase()); if(lowerSubComs[i].indexOf('" + block[i] + "') >= 0){badSubComs.push(fbSubComments[i]); badSubComD.push(fbSubComments[i].style.display); fbSubComments[i].style.display = 'none';}}"
+            code:   "fbSubComments = document.querySelectorAll('[aria-label = \"Comment reply\"]');" +
+                    "for (var i = 0; i < fbSubComments.length; i++){" +
+                    "   lowerSubComs.push(fbSubComments[i].innerHTML.toLowerCase());" +
+                    "   if(lowerSubComs[i].indexOf('" + block[i] + "') >= 0){" +
+                    "       badSubComs.push(fbSubComments[i]);" +
+                    "       badSubComD.push(fbSubComments[i].style.display);" +
+                    "       fbSubComments[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Descriptive Lists (dd)
         chrome.tabs.executeScript({
-            code: "dd = document.getElementsByTagName('dd'); for(var i = 0; i < dd.length; i++){lowerDD.push(dd[i].innerHTML.toLowerCase()); if(lowerDD[i].indexOf('" + block[i] + "') >= 0){badDD.push(dd[i]); badDDisp.push(dd[i].style.display); dd[i].style.display = 'none';}}"
+            code:   "dd = document.getElementsByTagName('dd');" +
+                    "for(var i = 0; i < dd.length; i++){" +
+                    "   lowerDD.push(dd[i].innerHTML.toLowerCase());" +
+                    "   if(lowerDD[i].indexOf('" + block[i] + "') >= 0){" +
+                    "       badDD.push(dd[i]);" +
+                    "       badDDisp.push(dd[i].style.display);" +
+                    "       dd[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Descriptive Lists (dt)
         chrome.tabs.executeScript({
-            code: "dt = document.getElementsByTagName('dt'); for (var i = 0; i < dt.length; i++){lowerDT.push(dt[i].innerHTML.toLowerCase()); if(lowerDT[i].indexOf('" + block[i] + "') >= 0){badDT.push(dt[i]); badTDisp.push(dt[i].style.display); dt[i].style.display = 'none';}}"
+            code:   "dt = document.getElementsByTagName('dt');" +
+                    "for (var i = 0; i < dt.length; i++){" +
+                    "   lowerDT.push(dt[i].innerHTML.toLowerCase());" +
+                    "   if(lowerDT[i].indexOf('" + block[i] + "') >= 0){" +
+                    "       badDT.push(dt[i]);" +
+                    "       badTDisp.push(dt[i].style.display);" +
+                    "       dt[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Checking twitter quoted tweets
         chrome.tabs.executeScript({
-            code: "qTweet = document.getElementsByClassName('QuoteTweet-container'); for(var i = 0; i<qTweet.length; i++){lowerQT.push(qTweet[i].innerHTML.toLowerCase()); if(lowerQT[i].indexOf('"+block[i]+"') >= 0){badQT.push(qTweet[i]); badQTDisp.push(qTweet[i].style.display); qTweet[i].style.display = 'none';}}"
+            code:   "qTweet = document.getElementsByClassName('QuoteTweet-container');" +
+                    "for(var i = 0; i<qTweet.length; i++){" +
+                    "   lowerQT.push(qTweet[i].innerHTML.toLowerCase());" +
+                    "   if(lowerQT[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badQT.push(qTweet[i]);" +
+                    "       badQTDisp.push(qTweet[i].style.display);" +
+                    "       qTweet[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
         // Checking H1
-        //window.alert('H1');
         chrome.tabs.executeScript({
-            code: "header1 = document.getElementsByTagName('h1'); for(var i = 0; i < header1.length; i++){lowerH1.push(header1[i].innerHTML.toLowerCase()); if(lowerH1[i].indexOf('"+block[i]+"') >= 0){badH1.push(header1[i]); badH1Disp.push(header1[i].style.display); header1[i].style.display = 'none';}}"
+            code:   "header1 = document.getElementsByTagName('h1');" +
+                    "for(var i = 0; i < header1.length; i++){" +
+                    "   lowerH1.push(header1[i].innerHTML.toLowerCase());" +
+                    "   if(lowerH1[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badH1.push(header1[i]);" +
+                    "       badH1Disp.push(header1[i].style.display);" +
+                    "       header1[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
-        // Youtube description
+        // Youtube description (DOESN'T WORK)
         chrome.tabs.executeScript({
-            code: "descYT = document.getElementByTagName('yt-formatted-string'); for(var i = 0; i < descYT.length; i++){lowerYTD.push(descYT[i].innerHTML.toLowerCase()); if(lowerYTD[i].indexOf('"+block[i]+"') >= 0){badYTD.push(descYT[i]); badYTDDisp.push(descYT[i].style.display); descYT[i].style.display = 'none';}}"
-            //lowerYTD = []; badYTD = []; badYTDDisp = [];
+            code:   "descYT = document.getElementByTagName('yt-formatted-string');" +
+                    "for(var i = 0; i < descYT.length; i++){" +
+                    "   lowerYTD.push(descYT[i].innerHTML.toLowerCase());" +
+                    "   if(lowerYTD[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badYTD.push(descYT[i]);" +
+                    "       badYTDDisp.push(descYT[i].style.display);" +
+                    "       descYT[i].style.display = 'none';" + 
+                    "   }" +
+                    "}"
         });
         // YouTube comments
         chrome.tabs.executeScript({
-            code: "commentYT = document.getElementsByClassName('style-scope ytd-comment-thread-renderer'); for(var i = 0; i < commentYT.length; i++){lowerYTC.push(commentYT[i].innerHTML.toLowerCase()); if(lowerYTC[i].indexOf('"+block[i]+"') >= 0){badYTC.push(commentYT[i]); badYTCDisp.push(commentYT[i].style.display); commentYT[i].style.display = 'none';}}"
+            code:   "commentYT = document.getElementsByClassName('style-scope ytd-comment-thread-renderer');" +
+                    "for(var i = 0; i < commentYT.length; i++){" +
+                    "   lowerYTC.push(commentYT[i].innerHTML.toLowerCase());" +
+                    "   if(lowerYTC[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badYTC.push(commentYT[i]);" +
+                    "       badYTCDisp.push(commentYT[i].style.display);" +
+                    "       commentYT[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
         });
-        // developer.chrome text in tables
+        // table contents
         chrome.tabs.executeScript({
-            code: "table = document.getElementsByTagName('td'); for(var i = 0; i < table.length; i++){lowerTable.push(table[i].innerHTML.toLowerCase()); if(lowerTable[i].indexOf('"+block[i]+"') >= 0){badTable.push(table[i]); badTableDisp.push(table[i].style.display); table[i].style.display = 'none';}}"
+            code:   "table = document.getElementsByTagName('td');" +
+                    "for(var i = 0; i < table.length; i++){"+
+                    "   lowerTable.push(table[i].innerHTML.toLowerCase());" +
+                    "   if(lowerTable[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badTable.push(table[i]);" +
+                    "       badTableDisp.push(table[i].style.display);" +
+                    "       table[i].style.display = 'none';" +
+                    "   }" +
+                    "}"
+        });
+        // table heading
+        chrome.tabs.executeScript({
+            code:   "tableHead = document.getElementsByTagName('th');" +
+                    "for(var i = 0; i < tableHead.length; i++){"+
+                    "   lowerTableHead.push(tableHead[i].innerHTML.toLowerCase());" +
+                    "   if(lowerTableHead[i].indexOf('"+block[i]+"') >= 0){" +
+                    "       badTableHead.push(tableHead[i]);" +
+                    "       badTableHeadDisp.push(tableHead[i].style.display);" +
+                    "       tableHead[i].style.display = 'none';"+
+                    "   }" +
+                    "}"
         });
     };
 };
 
+// This function shows hidden content. I'm thinking this isn't necessary, when the user turns off the extension they can reload the page to see it
+// Although this could be problematic for things like facebook were reloading the page changes what's on the page
+// Maybe show will be something for later versions
 function show(){
     // Paragraphs
     chrome.tabs.executeScript({
@@ -118,8 +221,12 @@ function show(){
     chrome.tabs.executeScript({
         code: "for(var j = 0; j < badYTC.length; j++){badYTC[j].style.display = badYTCDisp[j];}"
     });
-    // developer.chrome table
+    // table contents
     chrome.tabs.executeScript({
         code: "for(var j = 0; j < badTable.length; j++){badTable[j].style.display = badTableDisp[j];}"
+    });
+    // table header
+    chrome.tabs.executeScript({
+        code: "for(var j = 0; j < badTableHead.length; j++){badTableHead[j].style.display = badTableHeadDisp[j];}"
     });
 };
